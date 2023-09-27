16ce4a31c9e6305a2cf59c5811400ed5b55b108a
Raxmatillo <milodcomposer@gmail.com>
2023-09-27 23:36:51 +0500

    add structure files

---
+++ planner/models/users.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional, List
+from models.events import Event
+
+
+class User(BaseModel):
+    email: EmailStr
+    password: str
+    events: Optional[List[Event]]
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "fastapi@gmail.com",
+                "password": "strong!!!",
+                "events": [],
+            }
+        }
+
+
+
+class UserSignIn(BaseModel):
+    email: EmailStr
+    password: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "fastapi@gmail.com",
+                "password": "strong!!!",
+                "events": []
+            }
+        }
